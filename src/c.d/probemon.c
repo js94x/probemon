d6f325d1abf116d518bcddc73401237dd9cf9d74
solsTiCe d'Hiver <solstice.dhiver@gmail.com>
2020-05-20 16:57:00 +0200

    Refactoring in main
    add:
    * a parse_args function
    * an initialize_pcap function
    * a change_channel function

--- src/c.d/probemon.c
+++ src/c.d/probemon.c
@@ -98,18 +98,14 @@ void usage(void)
        );
 }
 
-int main(int argc, char *argv[])
+void parse_args(int argc, char *argv[], char **iface, uint8_t *channel, char **manuf_name, char **db_name, bool *option_stdout)
 {
-  char *iface = NULL;
+  int opt;
   char *option_channel = NULL;
   char *option_db_name = NULL;
   char *option_manuf_name = NULL;
-  char *db_name = NULL;
-  char *manuf_name = NULL;
-  int opt;
-  uint8_t channel;
 
-  option_stdout = false;
+  *option_stdout = false;
   while ((opt = getopt(argc, argv, "c:hi:d:m:sV")) != -1) {
     switch (opt) {
     case 'h':
@@ -117,7 +113,7 @@ int main(int argc, char *argv[])
       exit(EXIT_SUCCESS);
       break;
     case 'i':
-      iface = optarg;
+      *iface = optarg;
       break;
     case 'c':
       option_channel = optarg;
@@ -129,7 +125,7 @@ int main(int argc, char *argv[])
       option_manuf_name = optarg;
       break;
     case 's':
-      option_stdout = true;
+      *option_stdout = true;
       break;
     case 'V':
       printf("%s %s\nCopyright © 2020 solsTice d'Hiver\nLicense GPLv3+: GNU GPL version 3\n", NAME, VERSION);
@@ -144,7 +140,7 @@ int main(int argc, char *argv[])
     }
   }
 
-  if (iface == NULL) {
+  if (*iface == NULL) {
     fprintf(stderr, "Error: no interface selected\n");
     exit(EXIT_FAILURE);
   }
@@ -154,34 +150,48 @@ int main(int argc, char *argv[])
     fprintf(stderr, "Error: no channel defined\n");
     exit(EXIT_FAILURE);
   } else {
-    channel = (uint8_t)strtol(option_channel, NULL, 10);
+    *channel = (uint8_t)strtol(option_channel, NULL, 10);
+  }
+
+  if (option_db_name == NULL) {
+    *db_name = strdup(DB_NAME);
+  } else {
+    *db_name = strdup(option_db_name);
   }
 
   if (option_manuf_name == NULL) {
-    manuf_name = strdup(MANUF_NAME);
+    *manuf_name = strdup(MANUF_NAME);
   } else {
-    manuf_name = strdup(option_manuf_name);
+    *manuf_name = strdup(option_manuf_name);
   }
-  // look for manuf file and parse it into memory
-  if (access(manuf_name, F_OK) == -1 ) {
-    fprintf(stderr, "Error: can't find manuf file %s\n", manuf_name);
+}
+
+void change_channel(const char *iface, uint8_t channel)
+{
+  // look up for iw in system
+  char *iw;
+  if (access("/sbin/iw", F_OK) != -1) {
+    iw = strdup("/sbin/iw");
+  } else if (access("/usr/sbin/iw", F_OK) != -1) {
+    iw = strdup("/usr/sbin/iw");
+  } else if (access("/usr/bin/iw", F_OK) != -1) {
+    iw = strdup("/usr/bin/iw");
+  } else {
+    fprintf(stderr, "Error: couldn't find iw on system (in /sbin, /usr/sbin, /usr/bin)\n");
     exit(EXIT_FAILURE);
   }
-  printf(":: Parsing manuf file...\n");
-  fflush(stdout);
-  ouidb = parse_manuf_file(manuf_name, &ouidb_size);
-  if (ouidb == NULL) {
-    fprintf(stderr, "Error: can't parse manuf file\n");
+  // change the channel to listen on
+  char cmd[128];
+  snprintf(cmd, 128, "%s dev %s set channel %d", iw, iface, channel);
+  free(iw);
+  if (system(cmd)) {
+    fprintf(stderr, "Error: can't change to channel %d with iw on interface %s\n", channel, iface);
     exit(EXIT_FAILURE);
   }
+}
 
-  char **entries = parse_config_yaml(CONFIG_NAME, "ignored", &ignored_count);
-  ignored = parse_ignored_entries(entries, ignored_count);
-  for (int i=0; i<ignored_count; i++) {
-    free(entries[i]);
-  }
-  free(entries);
-
+void initiliaze_pcap(pcap_t **handle, const char *iface)
+{
   char errbuf[PCAP_ERRBUF_SIZE];
   // just check if iface is in the list of known devices
   pcap_if_t *devs = NULL;
@@ -203,50 +213,31 @@ int main(int argc, char *argv[])
     }
   }
 
-  // look up for iw in system
-  char *iw;
-  if (access("/sbin/iw", F_OK) != -1) {
-    iw = strdup("/sbin/iw");
-  } else if (access("/usr/sbin/iw", F_OK) != -1) {
-    iw = strdup("/usr/sbin/iw");
-  } else if (access("/usr/bin/iw", F_OK) != -1) {
-    iw = strdup("/usr/bin/iw");
-  } else {
-    fprintf(stderr, "Error: couldn't find iw on system (in /sbin, /usr/sbin, /usr/bin)\n");
-    exit(EXIT_FAILURE);
-  }
-  // change the channel to listen on
-  char cmd[128];
-  snprintf(cmd, 128, "%s dev %s set channel %d", iw, iface, channel);
-  free(iw);
-  if (system(cmd)) {
-    fprintf(stderr, "Error: can't change to channel %d with iw on interface %s\n", channel, iface);
-    exit(EXIT_FAILURE);
-  }
-
-  handle = pcap_create(iface, errbuf);
-  if (handle == NULL) {
+  *handle = pcap_create(iface, errbuf);
+  if (*handle == NULL) {
     fprintf(stderr, "Error: unable to create pcap handle: %s\n", errbuf);
     exit(EXIT_FAILURE);
   }
-  pcap_set_snaplen(handle, SNAP_LEN);
-  pcap_set_timeout(handle, 1000);
-  pcap_set_promisc(handle, 1);
+  pcap_set_snaplen(*handle, SNAP_LEN);
+  pcap_set_timeout(*handle, 1000);
+  pcap_set_promisc(*handle, 1);
 
-  if (pcap_activate(handle)) {
-    pcap_perror(handle, "Error: ");
+  if (pcap_activate(*handle)) {
+    pcap_perror(*handle, "Error: ");
     exit(EXIT_FAILURE);
   }
   // only capture packets received by interface
-  if (pcap_setdirection(handle, PCAP_D_IN)) {
-    pcap_perror(handle, "Error: ");
+  if (pcap_setdirection(*handle, PCAP_D_IN)) {
+    pcap_perror(*handle, "Error: ");
+    pcap_close(*handle);
     exit(EXIT_FAILURE);
   }
 
   int *dlt_buf, dlt_buf_len;
-  dlt_buf_len = pcap_list_datalinks(handle, &dlt_buf);
+  dlt_buf_len = pcap_list_datalinks(*handle, &dlt_buf);
   if (dlt_buf_len < 0) {
-    pcap_perror(handle, "Error: ");
+    pcap_perror(*handle, "Error: ");
+    pcap_close(*handle);
     exit(EXIT_FAILURE);
   }
   bool found = false;
@@ -258,12 +249,13 @@ int main(int argc, char *argv[])
   pcap_free_datalinks(dlt_buf);
 
   if (found) {
-    if (pcap_set_datalink(handle, DLT_IEEE802_11_RADIO)) {
-      pcap_perror(handle, "Error: ");
+    if (pcap_set_datalink(*handle, DLT_IEEE802_11_RADIO)) {
+      pcap_perror(*handle, "Error: ");
       exit(EXIT_FAILURE);
     }
   } else {
     fprintf(stderr, "Error: the interface %s does not support radiotap header or is not in monitor mode\n", iface);
+    pcap_close(*handle);
     exit(EXIT_FAILURE);
   }
 
@@ -271,18 +263,55 @@ int main(int argc, char *argv[])
   struct bpf_program bfp;
   char filter_exp[] = "type mgt subtype probereq";
 
-  if (pcap_compile(handle, &bfp, filter_exp, 1, PCAP_NETMASK_UNKNOWN) == -1) {
+  if (pcap_compile(*handle, &bfp, filter_exp, 1, PCAP_NETMASK_UNKNOWN) == -1) {
     fprintf(stderr, "Error: couldn't parse filter %s: %s\n",
-            filter_exp, pcap_geterr(handle));
-    ret = EXIT_FAILURE;
-    goto handle_failure;
+            filter_exp, pcap_geterr(*handle));
+    pcap_close(*handle);
+    exit(EXIT_FAILURE);
   }
-  if (pcap_setfilter(handle, &bfp) == -1) {
+  if (pcap_setfilter(*handle, &bfp) == -1) {
     fprintf(stderr, "Error: couldn't install filter '%s': %s\n",
-            filter_exp, pcap_geterr(handle));
+            filter_exp, pcap_geterr(*handle));
+    pcap_close(*handle);
     exit(EXIT_FAILURE);
   }
   pcap_freecode(&bfp);
+}
+
+int main(int argc, char *argv[])
+{
+  char *iface = NULL;
+  char *db_name = NULL;
+  char *manuf_name = NULL;
+  uint8_t channel;
+
+  parse_args(argc, argv, &iface, &channel, &manuf_name, &db_name, &option_stdout);
+
+  // look for manuf file and parse it into memory
+  if (access(manuf_name, F_OK) == -1 ) {
+    fprintf(stderr, "Error: can't find manuf file %s\n", manuf_name);
+    exit(EXIT_FAILURE);
+  }
+  printf(":: Parsing manuf file...\n");
+  fflush(stdout);
+  ouidb = parse_manuf_file(manuf_name, &ouidb_size);
+  if (ouidb == NULL) {
+    fprintf(stderr, "Error: can't parse manuf file\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // parse config.yaml file to populate ignored entries
+  char **entries = parse_config_yaml(CONFIG_NAME, "ignored", &ignored_count);
+  ignored = parse_ignored_entries(entries, ignored_count);
+  for (int i=0; i<ignored_count; i++) {
+    free(entries[i]);
+  }
+  free(entries);
+
+  initiliaze_pcap(&handle, iface);
+
+  // change channel with iw binary (fork)
+  change_channel(iface, channel);
 
   queue = new_queue(MAX_QUEUE_SIZE);
   // start the helper logger thread
@@ -304,12 +333,6 @@ int main(int argc, char *argv[])
   sigaction(SIGTERM, &act, NULL);
 
   clock_gettime(CLOCK_MONOTONIC, &start_ts_queue);
-
-  if (option_db_name == NULL) {
-    db_name = strdup(DB_NAME);
-  } else {
-    db_name = strdup(option_db_name);
-  }
 
   if (init_probemon_db(db_name, &db) != 0) {
     fprintf(stderr, "Error: can't initialize sqlite3 db %s\n", db_name);
@@ -358,7 +381,6 @@ logger_failure:
   pthread_mutex_destroy(&mutex_queue);
   pthread_cond_destroy(&cv);
 
-handle_failure:
   pcap_close(handle);
 
   free(db_name);
